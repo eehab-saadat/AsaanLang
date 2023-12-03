f86ccd70b39fb6c2090fb7c845564c8b7010a46d
eehab-saadat <eehab.saadat@gmail.com>
2023-12-04 01:16:11 +0500

    fixed tokenizer imports

--- modules/tokenizer.py
+++ modules/tokenizer.py
@@ -23,32 +23,32 @@ class Tokenizer:
                 self.tokenizer(exprs[i])
             else:
                 # check for arithmetic and assignment operator
-                if exprs[i] in tokens.OPERATOR.arithematic_op or exprs[i] in tokens.OPERATOR.assignment_op:
+                if exprs[i] in OPERATOR.arithematic_op or exprs[i] in OPERATOR.assignment_op:
 
-                    exprs[i] = tokens.OPERATOR(exprs[i])
+                    exprs[i] = OPERATOR(exprs[i])
                 # check for relational operators
-                elif exprs[i] in tokens.OPERATOR.relational_op:
+                elif exprs[i] in OPERATOR.relational_op:
 
-                    exprs[i]=tokens.OPERATOR(exprs[i])
+                    exprs[i]=OPERATOR(exprs[i])
 
                 # check boolean keywords
-                elif exprs[i] in tokens.BOOL_KW.bool_val:
+                elif exprs[i] in BOOL_KW.bool_val:
 
-                    exprs[i] = tokens.BOOL_KW(exprs[i])
+                    exprs[i] = BOOL_KW(exprs[i])
                 # check for conditional keywords
                 elif exprs[i] in self.conditional_keyword:
 
-                    exprs[i] = tokens.KEYWORD(exprs[i],self.conditional_keyword[exprs[i]])
+                    exprs[i] = KEYWORD(exprs[i],self.conditional_keyword[exprs[i]])
                     self.tokenizer(exprs[i+1])
-                    exprs[i+1] = tokens.CONDITIONAL_EXP(exprs[i+1])
+                    exprs[i+1] = CONDITIONAL_EXP(exprs[i+1])
                 # check for other keywords
                 elif exprs[i] in self.other_keywords:
 
-                    exprs[i] = tokens.KEYWORD(exprs[i], self.other_keywords[exprs[i]])
+                    exprs[i] = KEYWORD(exprs[i], self.other_keywords[exprs[i]])
                 # check for comments
                 elif type(exprs[i])==str:
                     if("#" in exprs[i]):
-                        exprs[i] = tokens.COMMENT(exprs[i])
+                        exprs[i] = COMMENT(exprs[i])
                     # symbol table checking
                     elif exprs[i].lower() in self.var_types:
                         # in case of multiple declarations throw error
@@ -58,13 +58,13 @@ class Tokenizer:
                         self.vars += exprs[i+1]
                         # checking variable type
                         if exprs[i].lower() == "number":
-                            self.symbol_table.append(types.NUMBER(exprs[i+1]))
+                            self.symbol_table.append(NUMBER(exprs[i+1]))
                         elif exprs[i].lower() == "ishariya":
-                            self.symbol_table.append(types.ISHARIYA(exprs[i+1]))
+                            self.symbol_table.append(ISHARIYA(exprs[i+1]))
                         elif exprs[i].lower() == "lafz":
-                            self.symbol_table.append(types.LAFZ(exprs[i+1]))
+                            self.symbol_table.append(LAFZ(exprs[i+1]))
                         elif exprs[i].lower() == "boolean":
-                            self.symbol_table.append(types.BOOLEAN(exprs[i+1]))
+                            self.symbol_table.append(BOOLEAN(exprs[i+1]))
         result = (self.expr_list, self.symbol_table)
         return result
 
