e7f5748611d1059ac7a0958fdca764bb85dbd4ec
eehab-saadat <eehab.saadat@gmail.com>
2023-12-04 01:29:00 +0500

    updated main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ with open("samples/sample.alif", "r") as file:
     source_code = file.read()
 
 expr_list = Lexer(source_code).lexicalize()
-print(expr_list)
-tokenized_list = Tokenizer(expr_list).tokenize()
-print(tokenized_list)
+print("Lexicalized AST:")
+print(expr_list, end="\n\n")
+tokenized_list, symbol_table = Tokenizer(expr_list).tokenize()
+print("Tokenized AST:")
+print(tokenized_list, end="\n\n")
+print("Symbol Table:")
+print(symbol_table)
