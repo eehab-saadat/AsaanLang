fbb500db77c12d60bab350b85fc1da2bd4a63026
akcena14 <116361254+akcena14@users.noreply.github.com>
2023-12-04 06:14:04 -0800

    Update runner.py

--- modules/runner.py
+++ modules/runner.py
@@ -4,11 +4,16 @@ import subprocess
 class Runner:
     input_file = ""
     output_file = ""
+    isCLI = False
 
-    def __init__(self, input_file: str, output_file: str) -> None:
+    def __init__(self, isCLI: bool = False ,input_file: str = "output/output.py", output_file: str= "output/output.txt") -> None:
         self.input_file = input_file
         self.output_file = output_file
+        self.isCLI = isCLI
 
     def run(self):
-        with open(self.output_file, "w+") as output:
-            subprocess.call(["python",f"./{self.input_file}"],stdout=output)
+        if not self.isCLI:
+            with open(self.output_file, "w+") as output:
+                subprocess.call(["python",f"./{self.input_file}"], stdout=output)
+        else:
+            subprocess.call(["python", f"./{self.input_file}"])
