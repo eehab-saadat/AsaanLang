40e96d8ab2e302974510c424777caaf99b0b0a6d
Uffan Mehmood Khan <52833092+UffanMehmoodKhan@users.noreply.github.com>
2023-12-06 18:06:39 +0500

    Update generator.py

--- modules/generator.py
+++ modules/generator.py
@@ -1,58 +1,26 @@
-import sys
-sys.path.insert(0, "D:\AsaanLang\modules") #replace with appropriate module filepath
-from modules.models.statements import ASSIGNMENT_STATEMENT
-from models.statements import ASSIGNMENT_STATEMENT
+# Imports
+from typing import TextIO # type hinting for file object
+from models.statements import *
 
-# from models.statements import __str__
-   
-
+# CodeGenerator class
 class CodeGenerator:
-    AST = []
-    indent = 0
+    AST = [] # parsed AST
+    filename = "" # output filename
+
+    # constructor
+    def __init__(self, AST: list, filename: str = "output/output.py") -> None:
+        self.AST = AST # parsed AST
+        self.filename = filename # output filename
     
-    def __init__(self, AST):
-        self.AST = AST
-
-    def indent_level(self) -> int:      #returns current indent level
-        return self.indent
+    # generate python code from the parsed AST
+    def generate(self) -> None:
+        with open(self.filename, "w") as file:
+            self.generate_recursively(file, self.AST)
     
-    def indent_change(self) ->int:      #increments indent level
-        self.indent = self.indent + 1
-    
-    def generate(self, filename: str = "") -> None:
-        indent_num = self.indent_level()                #stores indent level in the beginning of the code generation #at which indent is 0
-        print_list(self.AST, indent_num=indent_num)     #sends the list to function to be indented and replaced
-
-
-def print_list(the_list, indent_num):
-
-    secondfile = open(f'D:\AsaanLang\output\output.py', 'a')
-    for each_item in the_list:
-        if isinstance(each_item, list):
-            secondfile.seek(0,0) 
-            secondfile.write('\n')
-            print_list(each_item, indent_num = indent_num + 1)
-        else:
-            secondfile.write(('\t' * indent_num) + each_item.__str__() + '\n') 
-    secondfile.close()
-
-        
-a = ASSIGNMENT_STATEMENT("a", "2")
-print(a)
-
-
-'''
-from output.py cache lines
-'''
-#import sys, ast, re
-# sys.path.insert(0, "D:\AsaanLang\modules")
-
-# from lexer import Lexer 
-# from parser import Parser
-# from tokenizer import Tokenizer
-
-# import models 
-# from models import __str__
-
-
-    
+    # recursive function to generate python code from the AST
+    def generate_recursively(self, file: TextIO, AST: list, indent: int = 0) -> None: 
+        for item in AST: # iterate through the parsed AST
+            if isinstance(item, list): # if item is a list, then it is a block of code
+                self.generate_recursively(file, item, indent + 1) # recursive call
+            else:
+                file.write("\t" * indent + str(item) + "\n") # write to file
