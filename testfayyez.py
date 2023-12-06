8c5cfd66124637684ff01996e5b8be5004c9cde5
Fayyez <fayyez@gmail.com>
2023-12-07 03:06:11 +0500

    deleted testfile

--- testfayyez.py
+++
@@ -1,6 +0,0 @@
-from modules.models.statements import ASSIGNMENT_STATEMENT
-from modules.generator import CodeGenerator
-
-a = ASSIGNMENT_STATEMENT("a","10")
-ast = [a]
-CodeGenerator(ast).generate()
